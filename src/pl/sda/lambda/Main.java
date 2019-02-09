54952cd9214c152f4e651993f062803b31c3e7d5
Marcin Siatka <siatka131@gmail.com>
2019-02-09 10:20:51 +0100

    testy1

--- src/pl/sda/lambda/Main.java
+++ src/pl/sda/lambda/Main.java
@@ -22,6 +22,7 @@ public class Main {
         Consumer<Integer> integerConsumer = n -> System.out.println(n) ;
         numbers.forEach(integerConsumer);
 
+        numbers.forEach(s -> System.out.println(s));
         numbers.forEach(System.out::println);
 
         List<Movie> movies = Arrays.asList(
@@ -34,13 +35,18 @@ public class Main {
                         "Jan",
                         LocalDate.of(2012,02,12),
                         20,
+                        Arrays.asList("Michal", "Maciek")),
+                 new Movie("Tytul3",
+                        "Jan",
+                        LocalDate.of(2012,02,12),
+                        125,
                         Arrays.asList("Michal", "Maciek"))
         );
 
 
         List<String> titles = movies
                 .stream()
-                .map(f -> f.getTitle())
+                .map(f -> f.getTitle())  //mozna sie dostac za pomoca referencji ::
                 .collect(Collectors.toList());
 
         //drugi stream - lista directors
@@ -56,5 +62,21 @@ public class Main {
                 .filter(f -> f.getPrice() > 50)
                 .filter(f -> f.getTitle().length() > 5)
                 .collect(Collectors.toList());
+
+        movieList.forEach(p-> System.out.println(p.getTitle() + p.getPrice()));
+        movieList.forEach(System.out::println);
+
+        //zwroc liste tytulow filmow ktorych getPrice > 10 i < 30
+
+        List<String> movieList1 = movies.stream()
+                .filter(f -> f.getPrice() > 10)
+                .filter(f -> f.getPrice() < 30)
+                .map(f-> f.getTitle())
+                .collect(Collectors.toList());
+        System.out.println(" _____ ");
+       movieList1.forEach(System.out::println);
+
+
+
     }
 }
